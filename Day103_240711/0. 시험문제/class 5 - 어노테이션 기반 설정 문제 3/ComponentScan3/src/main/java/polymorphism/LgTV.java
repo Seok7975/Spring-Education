84f0d96fdc5d471249e199ac0c9f1b5c97de2d1a
parkhongseok <seok7975@naver.com>
2024-07-11 18:08:42 +0900

    -

---
+++ Day103_240711/0. 시험문제/class 5 - 어노테이션 기반 설정 문제 3/ComponentScan3/src/main/java/polymorphism/LgTV.java
@@ -0,0 +1,26 @@
+package polymorphism;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+public class LgTV implements TV {
+	
+	private Speaker speaker;
+	
+	public LgTV() {
+		System.out.println("LgTV 생성자");
+	}
+	public void powerOn(){
+		System.out.println("LgTV --- 전원 켠다.");
+	}
+	public void powerOff(){
+		System.out.println("LgTV --- 전원 끈다.");
+	}
+	public void volumeUp(){
+		speaker.volumeUp();
+	}
+	public void volumeDown(){
+		speaker.volumeDown();
+	}
+}
