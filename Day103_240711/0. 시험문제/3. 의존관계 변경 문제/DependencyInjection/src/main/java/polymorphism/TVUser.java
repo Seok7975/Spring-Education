dd4045cf765ca124d1374578b5d6f8248de066fb
parkhongseok <seok7975@naver.com>
2024-07-11 18:48:02 +0900

    -

---
+++ Day103_240711/0. 시험문제/3. 의존관계 변경 문제/DependencyInjection/src/main/java/polymorphism/TVUser.java
@@ -0,0 +1,17 @@
+package polymorphism;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class TVUser {
+
+	public static void main(String[] args) {
+		
+		TV tv = (TV)factory.getBean("tv");
+		tv.powerOn();
+		tv.volumeUp();
+		tv.volumeDown();	
+		tv.powerOff();
+		
+	}
+}
---
+++ Day103_240711/Day2 class 2/AOP 용어 및 기본 설정 4 예제/SpringBoard/src/main/java/com/springbook/biz/board/impl/BoardServiceAOPTest.java
@@ -0,0 +1,15 @@
+package com.springbook.biz.board.impl;
+
+public interface BoardServiceAOPTest {
+
+	void hi();
+
+	int good();
+
+	String nice();
+
+	void add(int num1, int num2);
+	
+	void morninghi();
+
+}
